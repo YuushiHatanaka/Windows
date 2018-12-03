4f14c168ada70acf7aaea1c51a4c96f1f3ceb79e
YuushiHatanaka <44627879+YuushiHatanaka@users.noreply.github.com>
2018-12-04 08:44:53 +0900

    Update NtpClient.cs

--- Common/Net/NtpClient.cs
+++ Common/Net/NtpClient.cs
@@ -1,1 +1,12 @@
-C#
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.net
+{
+public class NtpClient
+{
+};
+};
